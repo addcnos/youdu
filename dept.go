1937ac399a99549612806e5575cdaa613c83c90d
Flc゛ <four_leaf_clover@foxmail.com>
2023-08-03 13:43:44 +0800

    refactor: youdu 2.x - dept

---
+++ dept.go
@@ -0,0 +1,39 @@
+package youdu
+
+import (
+	"context"
+	"net/http"
+	"strconv"
+)
+
+type DeptList struct {
+	ID       interface{} `json:"id"`
+	Name     string      `json:"name"`
+	ParentId interface{} `json:"parentId"`
+	SortId   interface{} `json:"sortId"`
+}
+
+type DeptListResponse struct {
+	DeptList []DeptList `json:"deptList"`
+}
+
+func (c *Client) GetDeptList(ctx context.Context, id ...int) (response DeptListResponse, err error) {
+	opts := []requestOption{
+		withRequestAccessToken(),
+		withRequestEncrypt(),
+	}
+
+	if len(id) > 0 {
+		opts = append(opts, withRequestParamsKV("id", strconv.Itoa(id[0])))
+	} else {
+		opts = append(opts, withRequestParamsKV("id", "0"))
+	}
+
+	req, err := c.newRequest(ctx, http.MethodGet, "/cgi/dept/list", opts...)
+	if err != nil {
+		return
+	}
+
+	err = c.sendRequest(req, &response, withResponseDecrypt())
+	return
+}
