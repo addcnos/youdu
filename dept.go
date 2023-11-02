cc36ca83b5c71b78e861be91889eb0c41a52f695
ChanVon <fengchenorz@gmail.com>
2023-11-02 14:50:46 +0800

    feat(dept): Add dept `getid` (#24)
    * feat:dept.go@GetDeptIdByAlias
    
    * feat:dept.go@GetDeptIdByAlias
    
    * feat:dept.go@GetDeptIdByAlias
    
    * feat:dept.go@GetDeptIdByAlias

--- dept.go
+++ dept.go
@@ -17,6 +17,19 @@ type DeptListResponse struct {
 	DeptList []DeptItem `json:"deptList"`
 }
 
+type DeptAliasItem struct {
+	Id    int    `json:"id"`
+	Alias string `json:"alias"`
+}
+
+type DeptAliasListResponse struct {
+	AliasList []DeptAliasItem `json:"aliasList"`
+}
+
+type DeptIdByAliasResponse struct {
+	Id int `json:"id"`
+}
+
 func (c *Client) GetDeptList(ctx context.Context, id ...int) (response DeptListResponse, err error) {
 	opts := []requestOption{
 		withRequestAccessToken(),
@@ -30,6 +43,37 @@ func (c *Client) GetDeptList(ctx context.Context, id ...int) (response DeptListR
 	}
 
 	req, err := c.newRequest(ctx, http.MethodGet, "/cgi/dept/list", opts...)
+	if err != nil {
+		return
+	}
+
+	err = c.sendRequest(req, &response, withResponseDecrypt())
+	return
+}
+
+func (c *Client) GetDeptAliasList(ctx context.Context) (response DeptAliasListResponse, err error) {
+	opts := []requestOption{
+		withRequestAccessToken(),
+		withRequestEncrypt(),
+	}
+
+	req, err := c.newRequest(ctx, http.MethodGet, "/cgi/dept/getid", opts...)
+	if err != nil {
+		return
+	}
+
+	err = c.sendRequest(req, &response, withResponseDecrypt())
+	return
+}
+
+func (c *Client) GetDeptIdByAlias(ctx context.Context, alias string) (response DeptIdByAliasResponse, err error) {
+	opts := []requestOption{
+		withRequestAccessToken(),
+		withRequestEncrypt(),
+		withRequestParamsKV("alias", alias),
+	}
+
+	req, err := c.newRequest(ctx, http.MethodGet, "/cgi/dept/getid", opts...)
 	if err != nil {
 		return
 	}
