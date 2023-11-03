bbfcd188f77447d057987ffdc50a8af7fa6665d3
fengchen <fengchenorz@gmail.com>
2023-11-03 22:37:32 +0800

    feat:get user enable state

--- user.go
+++ user.go
@@ -39,6 +39,10 @@ type DeptUserListResponse struct {
 	UserList []UserItem `json:"userList"`
 }
 
+type UserEnableStateResponse struct {
+	EnableState int `json:"enableState"`
+}
+
 func (c *Client) GetUser(ctx context.Context, userId string) (response UserResponse, err error) {
 	req, err := c.newRequest(ctx, http.MethodGet, "/cgi/user/get",
 		withRequestAccessToken(),
@@ -72,6 +76,20 @@ func (c *Client) GetDeptUserSimpleList(ctx context.Context, deptId int) (respons
 		withRequestAccessToken(),
 		withRequestEncrypt(),
 		withRequestParamsKV("deptId", strconv.Itoa(deptId)),
+	)
+	if err != nil {
+		return
+	}
+
+	err = c.sendRequest(req, &response, withResponseDecrypt())
+	return
+}
+
+func (c *Client) GetUserEnableState(ctx context.Context, userId string) (response UserEnableStateResponse, err error) {
+	req, err := c.newRequest(ctx, http.MethodGet, "/cgi/user/enable/state",
+		withRequestAccessToken(),
+		withRequestEncrypt(),
+		withRequestParamsKV("userId", userId),
 	)
 	if err != nil {
 		return
